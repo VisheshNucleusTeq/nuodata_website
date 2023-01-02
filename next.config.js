17f7054327e4bb81d1a842340f6b4a29a053777e
aishwary-nucleusteq <116073314+aishwary-nucleusteq@users.noreply.github.com>
2023-01-02 14:53:23 +0530

    update router

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
-  exportPathMap: async function (defaultPathMap) {
-    return {
-      '/demo/index.html': { page: '/demo' },
-    };
-  }
 };
 
 module.exports = nextConfig;
