c7b98c85ba34f97da0b4373268d398a2724d6c8e
aishwary-nucleusteq <116073314+aishwary-nucleusteq@users.noreply.github.com>
2023-01-14 09:19:10 +0530

    design page

--- components/home/datamodernizationarchitecture.js
+++ components/home/datamodernizationarchitecture.js
@@ -11,11 +11,11 @@ export default function DataModernizationArchitecture({ HomeCss }) {
         <h1>
           <span>Data Modernization Architecture</span>
         </h1>
-        <p>
+        {/* <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s
-        </p>
+        </p> */}
       </Col>
       <Col offset={1} span={22} className={HomeCss.infoRowSystemView}>
         <div className={HomeCss.infoRowSystemTextView} />
