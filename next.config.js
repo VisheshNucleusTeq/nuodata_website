7b82ec074a072d769656257c798152c1103cc719
aishwary-nucleusteq <116073314+aishwary-nucleusteq@users.noreply.github.com>
2023-02-17 17:35:03 +0530

    udpate next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   trailingSlash: true,
-  staticPageGenerationTimeout:1200
-}
+  staticPageGenerationTimeout: 1200,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/how-it-works": { page: "/how-it-works" },
+    };
+  },
+};
