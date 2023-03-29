2dcc16b3dbc17c35d62e2e1e367bda8518b95a46
aishwary-nucleusteq <116073314+aishwary-nucleusteq@users.noreply.github.com>
2023-03-29 15:43:12 +0530

    Delete deploy.cmd

--- deploy.cmd
+++
@@ -1,5 +0,0 @@
-git config --global user.name "aishwary-nucleusteq"
-git config --global user.email "aishwary.rathore@nucleusteq.com"
-git add .
-git commit -m "latest --deploy"
-git push origin dev_uat
