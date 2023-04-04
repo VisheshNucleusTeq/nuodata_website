7052df6a2f40ae0e3d8982907e310db8ecb23576
aishwary-nucleusteq <aishwary.rathore@nucleusteq.com>
2023-04-04 12:58:06 +0530

    latest --deploy

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,23 @@
 import Home from "../components/home";
+import {useRouter} from 'next/router';
+import { useEffect } from "react";
 
 export default function Main(props) {
+  console.log(123)
+  const router = useRouter()
+
+  useEffect(() => {
+    if(router.pathname != "/"){
+      alert(router.pathname)
+    }
+  },[router.pathname])
+
   return (
     <>
       <title>NuoData | Home</title>
+      <h2>pathname:- {router.pathname}</h2>
+            {/* <h2>query:- {router.query}</h2>
+            <h2>asPath:- {router.asPath}</h2> */}
       <Home />
     </>
   );
