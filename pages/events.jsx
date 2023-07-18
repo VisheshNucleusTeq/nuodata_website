ea82e4d5dc4dbdc75d5dfea9a369833a3c187ddb
aishwary-nucleusteq <aishwary.rathore@nucleusteq.com>
2023-07-18 13:21:00 +0530

    add event admin --deploy

--- pages/events.jsx
+++ pages/events.jsx
@@ -1,5 +1,5 @@
 import Events from "../components/events";
-import EventsCss from "../styles/events.module.css"
+import EventsCss from "../styles/event.module.css"
 function New_Events(props) {
   return (
     <>
