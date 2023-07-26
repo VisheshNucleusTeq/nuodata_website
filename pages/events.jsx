0dba61716149c7db282e1de75214f3b3bb918137
aishwary-nucleusteq <aishwary.rathore@nucleusteq.com>
2023-07-26 15:52:39 +0530

    update event view website --deploy

--- pages/events.jsx
+++ pages/events.jsx
@@ -4,6 +4,21 @@ function New_Events(props) {
   return (
     <>
       <title>NuoData | New Events</title>
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="NuoData" />
+      <meta
+        property="og:description"
+        content="ONE PLATFORM FOR ALL ENTERPRISE DATA MODERNIZATION & MANAGEMENT NEEDS."
+      />
+      <meta property="og:site_name" content="NuoData" />
+      <meta property="og:url" content="https://nuodata.io/" />
+      <meta
+        property="og:image"
+        itemProp="image"
+        content="https://nuodata.io/logo.png"
+      />
+      <meta property="og:image:type" content="image/png" />
       <Events EventsCss={EventsCss}/>
     </>
   );
