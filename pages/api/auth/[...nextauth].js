dd7b89474de31a2702b0ca534d78621e0bad1e51
Reena Anthony <reena.anthony@nucleusteq.com>
2024-02-13 21:15:21 +0530

    keycloack implementation changes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import KeycloakProvider from "next-auth/providers/keycloak";
 
 export default NextAuth({
   providers: [
-    GoogleProvider({
-      clientId:
-        "995061213404-vbdmb63jpqa8ua22u5jhlc9t9f4r8h3m.apps.googleusercontent.com", //process.env.GOOGLE_CLIENT_ID,
-      clientSecret: "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", //process.env.GOOGLE_CLIENT_SECRET
+    KeycloakProvider({
+      clientId: "nuodate-auth", 
+      clientSecret: "ZSWUY1Yf4jJiYgZDIDx9TOnZ6zzJBE57", 
+      issuer: "https://api.dev.nuodata.io/identity/realms/nuodata",
     }),
   ],
   pages: {
@@ -15,10 +15,10 @@ export default NextAuth({
 
   callbacks: {
     async signIn({ account, profile }) {
-      if (account.provider === "google") {
-        return profile.email_verified && profile.email.endsWith("@example.com")
+      if (account.provider === "keycloak") {
+        return profile.email_verified && profile.email.endsWith("@example.com");
       }
-      return true // Do different verification for other providers that don't have `email_verified`
+      return true;
     },
     async session({ session, token, user }) {
       session.user.username = session.user.name;
@@ -27,38 +27,3 @@ export default NextAuth({
     },
   },
 });
-
-// import NextAuth from "next-auth/next";
-// import GoogleProvider from "next-auth/providers/google";
-
-// export default NextAuth({
-//   providers: [
-//     GoogleProvider({
-//       clientId:
-//         "995061213404-vbdmb63jpqa8ua22u5jhlc9t9f4r8h3m.apps.googleusercontent.com", //process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: "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", //process.env.GOOGLE_CLIENT_SECRET
-//     }),
-//   ],
-//   secret: "nuodata@123#",
-//   callbacks: {
-//     async session({ session, token, user }) {
-//       session.jti = token.jti;
-//       return session;
-//     },
-//     // async jwt({ token, user, account, profile, isNewUser }) {
-//     //   if (user) {
-//     //     token.id = user.id;
-//     //   }
-//     //   if (account) {
-//     //     token.accessToken = account.access_token;
-//     //   }
-//     //   return token;
-//     // },
-//   },
-//   session: {
-//     strategy: "jwt",
-//   },
-//   pages: {
-//     signIn: '/auth/signin',
-//   },
-// });
