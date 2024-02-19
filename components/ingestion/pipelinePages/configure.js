4b28fee2a41db72dbf403756c937eb85c5efcf09
Anchal-sharma-nucleusTeq <anchal.sharma@nucleusteq.com>
2024-02-19 18:25:37 +0530

    modify and integrated getapi in configurepage

--- components/ingestion/pipelinePages/configure.js
+++ components/ingestion/pipelinePages/configure.js
@@ -1,29 +1,41 @@
 import {
-  Input,
-  Select,
-  Row,
-  Col,
-  Form,
   Button,
-  Typography,
-  Space,
+  Col,
+  DatePicker,
+  Form,
+  Input,
   Radio,
-  Badge,
-  Progress,
+  Row,
+  Select,
+  Space,
   Switch,
+  Typography,
 } from "antd";
-import React, { useEffect, useState } from "react";
 import cronstrue from "cronstrue";
+import React, { useEffect, useState } from "react";
+import { fetch_retry_get } from "../../../network/api-manager";
+import { ENVDETAILS, GETWORKSPACEENV } from "../../../network/apiConstants";
+import { compareObjects } from "../../helper/compareObject";
 import timeZone from "../../helper/timeZone";
-import { SlackCircleFilled } from "@ant-design/icons";
-import Icon from "@ant-design/icons/lib/components/Icon";
 
-const Configure = ({ ingestionCss }) => {
+const Configure = ({
+  ingestionCss,
+  setSelectedTab,
+  pipelineDetails,
+  workspace,
+}) => {
+  const [runOnSchedule, SetRunOnSchedule] = useState(true);
+  const [startDate, setStartDate] = useState();
+  const [environmentDetails, setEnvironmentDetails] = useState({});
+  const [environmentList, setEnvironmentList] = useState([]);
+  const [selectedRuntimeEnvironmentId, setSelectedRuntimeEnvironmentId] =
+    useState(pipelineDetails?.runtime_env_id);
   const [cronExpression, setCronExpression] = useState("");
   const [isCronExpressionValid, setIsCronExpressionValid] = useState(
     cronExpression != ""
   );
   const [description, setDescription] = useState("");
+  const [scheduleTimezone, setScheduleTimezone] = useState();
   const [selectedScheduleOption, setSelectedScheduleOption] = useState("Day");
   const scheduleOption = ["Day", "Custom", "Minute", "Hour", "Week"];
   const [selectedOptionValue, setSelectedOptionValue] = useState({
@@ -34,19 +46,8 @@ const Configure = ({ ingestionCss }) => {
     Week: { hour: "0", minute: "0", dayOfWeek: "0" },
   });
 
-  const [params, setParams] = useState({
-    "spark.executor.cores": "4",
-    "spark.executor.memory": "14G",
-    "spark.driver.memory": "14G",
-    "spark.driver.cores": "4",
-    "spark.executor.instances": "3",
-    "spark.dynamicAllocation.maxExecutors": "100",
-    "spark.emr-serverless.driver.disk": "20G",
-    "spark.emr-serverless.executor.disk": "20G",
-    "spark.dynamicAllocation.minExecutors": "0",
-    "spark.dynamicAllocation.maxExecutors": "100",
-    "spark.dynamicAllocation.initialExecutors": "3",
-  });
+  const [dateFormat] = useState("YYYY-MM-DD");
+  const [params, setParams] = useState({});
   const hoursOptions = Array.from({ length: 24 }, (_, i) => ({
     label: i.toString().padStart(2, "0"),
     value: i.toString().padStart(2, "0"),
@@ -182,57 +183,119 @@ const Configure = ({ ingestionCss }) => {
             />
           </Form.Item>
         </Col>
+        {/* <Col offset={2} span={4}>
+          <Form.Item
+            className={ingestionCss.antFormItem}
+            label={"Start Date"}
+            labelAlign={"left"}
+            required
+          >
+            <DatePicker
+              className={ingestionCss.textInput}
+              name="startDate"
+              format={"YYYY-MM-DD"}
+            />
+          </Form.Item>
+        </Col> */}
       </>
     ),
     Custom: (
-      <Col span={4}>
-        <Form.Item
-          className={ingestionCss.antFormItem}
-          label={"Cron"}
-          labelAlign={"left"}
-          required
-        >
-          <Input
-            placeholder="****?"
-            className={ingestionCss.input}
-            onChange={(e) => setCronExpression(e.target.value)}
-          />
-        </Form.Item>
-      </Col>
+      <>
+        <Col span={4}>
+          <Form.Item
+            className={ingestionCss.antFormItem}
+            label={"Cron"}
+            labelAlign={"left"}
+            required
+          >
+            <Input
+              placeholder="****?"
+              className={ingestionCss.input}
+              onChange={(e) => setCronExpression(e.target.value)}
+            />
+          </Form.Item>
+        </Col>
+        {/* <Col offset={2} span={4}>
+          <Form.Item
+            className={ingestionCss.antFormItem}
+            label={"Start Date"}
+            labelAlign={"left"}
+            required
+          >
+            <DatePicker
+              className={ingestionCss.textInput}
+              name="startDate"
+              format={"YYYY-MM-DD"}
+            />
+          </Form.Item>
+        </Col> */}
+      </>
     ),
     Minute: (
-      <Col span={4}>
-        <Form.Item
-          className={ingestionCss.antFormItem}
-          label={"Minute"}
-          labelAlign={"left"}
-          required
-        >
-          <Select
-            className={ingestionCss.inputSelect}
-            onChange={handleMinuteChange}
-            options={minutesOptions}
-            value={selectedOptionValue[selectedScheduleOption]["Minute"]}
-          />
-        </Form.Item>
-      </Col>
+      <>
+        <Col span={4}>
+          <Form.Item
+            className={ingestionCss.antFormItem}
+            label={"Minute"}
+            labelAlign={"left"}
+            required
+          >
+            <Select
+              className={ingestionCss.inputSelect}
+              onChange={handleMinuteChange}
+              options={minutesOptions}
+              value={selectedOptionValue[selectedScheduleOption]["Minute"]}
+            />
+          </Form.Item>
+        </Col>
+        {/* <Col offset={2} span={4}>
+          <Form.Item
+            className={ingestionCss.antFormItem}
+            label={"Start Date"}
+            labelAlign={"left"}
+            required
+          >
+            <DatePicker
+              className={ingestionCss.textInput}
+              name="startDate"
+              format={"YYYY-MM-DD"}
+            />
+          </Form.Item>
+        </Col> */}
+      </>
     ),
     Hour: (
-      <Col span={4}>
-        <Form.Item
-          className={ingestionCss.antFormItem}
-          label={"Minute"}
-          labelAlign={"left"}
-          value={selectedOptionValue[selectedScheduleOption]["Minute"]}
-          required
-        >
-          <Select
-            className={ingestionCss.inputSelect}
-            onChange={handleMinuteChange}
-            options={minutesOptions}
-          />
-        </Form.Item>
-      </Col>
+      <>
+        <Col span={4}>
+          <Form.Item
+            className={ingestionCss.antFormItem}
+            label={"Minute"}
+            labelAlign={"left"}
+            value={selectedOptionValue[selectedScheduleOption]["Minute"]}
+            required
+          >
+            <Select
+              className={ingestionCss.inputSelect}
+              onChange={handleMinuteChange}
+              options={minutesOptions}
+            />
+          </Form.Item>
+        </Col>
+        {/* <Col offset={2} span={4}>
+          <Form.Item
+            className={ingestionCss.antFormItem}
+            label={"Start Date"}
+            labelAlign={"left"}
+            required
+          >
+            <DatePicker
+              className={ingestionCss.textInput}
+              name="startDate"
+              format={"YYYY-MM-DD"}
+            />
+          </Form.Item>
+        </Col> */}
+      </>
     ),
     Week: (
       <>
@@ -266,6 +329,20 @@ const Configure = ({ ingestionCss }) => {
             />
           </Form.Item>
         </Col>
+        {/* <Col offset={2} span={4}>
+          <Form.Item
+            className={ingestionCss.antFormItem}
+            label={"Start Date"}
+            labelAlign={"left"}
+            required
+          >
+            <DatePicker
+              className={ingestionCss.textInput}
+              name="startDate"
+              format={"YYYY-MM-DD"}
+            />
+          </Form.Item>
+        </Col> */}
         <Col span={24}>
           <Form.Item
             className={ingestionCss.antFormItem}
@@ -288,16 +365,57 @@ const Configure = ({ ingestionCss }) => {
       </>
     ),
   };
+  const handleSaveNDeploy = () => {
+    let saveNDeployData = {
+      runtime_env_id: selectedRuntimeEnvironmentId,
+      runtime_engine: pipelineDetails?.runtime_engine,
+    };
+    if (runOnSchedule && isCronExpressionValid) {
+      if (!compareObjects(params, environmentDetails?.params)) {
+        saveNDeployData.params = params;
+      }
+      saveNDeployData.start_date = startDate;
+      saveNDeployData.schedule_cron_expression = cronExpression;
+      saveNDeployData.schedule_timezone = scheduleTimezone;
+    }
+    console.log(saveNDeployData);
+  };
+
+  const getEnvList = async () => {
+    const authData = JSON.parse(localStorage.getItem("authData"));
+    const envList = await fetch_retry_get(
+      `${GETWORKSPACEENV}${workspace}?org_id=${authData.orgId}`
+    );
+    setEnvironmentList(envList?.data);
+  };
+  const getEnvData = async () => {
+    const envDetails = await fetch_retry_get(
+      `${ENVDETAILS}${selectedRuntimeEnvironmentId}`
+    );
+    if (envDetails.success) {
+      setEnvironmentDetails(envDetails?.data);
+      setParams(envDetails?.data?.params);
+    }
+  };
+
   useEffect(() => {
     handleParse();
   }, [cronExpression]);
-
+  useEffect(() => {
+    getEnvList();
+  }, []);
+  useEffect(() => {
+    if (selectedRuntimeEnvironmentId !== undefined) getEnvData();
+  }, [selectedRuntimeEnvironmentId]);
   return (
     <>
       <Row className={ingestionCss.configureFormHeadingRow}>
         <Col>
           <Typography.Text className={ingestionCss.configureFormHeading}>
-            Engine:<span style={{ color: "var(--Primary)" }}> {"Spark"}</span>
+            Engine:
+            <span style={{ color: "var(--Primary)" }}>
+              {pipelineDetails?.runtime_engine}
+            </span>
           </Typography.Text>
         </Col>
       </Row>
@@ -322,80 +440,108 @@ const Configure = ({ ingestionCss }) => {
               <Switch
                 style={{ width: "4vw", height: "1.75vw" }}
                 defaultChecked
+                onChange={(checked) => {
+                  SetRunOnSchedule(checked);
+                }}
               ></Switch>
             </Form.Item>
           </Col>
-
-          <Col span={24}>
-            <Row gutter={16}>
-              <Col span={8}>
+          {runOnSchedule && (
+            <>
+              <Col span={24}>
+                <Row gutter={16}>
+                  <Col span={7}>
+                    <Form.Item
+                      className={ingestionCss.antFormItem}
+                      label={"Start Date"}
+                      labelAlign={"left"}
+                      // required
+                    >
+                      <DatePicker
+                        className={ingestionCss.textInput}
+                        format={dateFormat}
+                        onChange={(date) => {
+                          setStartDate(date?.toDate());
+                        }}
+                      />
+                    </Form.Item>
+                  </Col>
+                  <Col offset={1} span={8}>
+                    <Form.Item
+                      className={ingestionCss.antFormItem}
+                      label={"Schedule"}
+                      labelAlign={"left"}
+                      required
+                    >
+                      <Select
+                        className={ingestionCss.inputSelect}
+                        value={selectedScheduleOption}
+                        placeholder={"select"}
+                        onChange={(option) => {
+                          setCronExpression("");
+                          setSelectedScheduleOption(option);
+                        }}
+                        options={scheduleOption.map((val) => {
+                          return { label: val, value: val };
+                        })}
+                      />
+                    </Form.Item>
+                  </Col>
+                  {optionChild[selectedScheduleOption]}
+                  <Col span={24} style={{ marginBottom: "8px" }}>
+                    <Space size={8}>
+                      <Typography.Text className={ingestionCss.cronDescription}>
+                        {isCronExpressionValid && cronExpression !== "" && (
+                          <>
+                            <svg
+                              width="18"
+                              height="18"
+                              viewBox="0 0 18 18"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M5 9.17407L7.14645 11.4451C7.34171 11.6516 7.65829 11.6516 7.85355 11.4451L13 6M17 9C17 13.4183 13.4183 17 9 17C4.58172 17 1 13.4183 1 9C1 4.58172 4.58172 1 9 1C13.4183 1 17 4.58172 17 9Z"
+                                stroke="#309133"
+                                stroke-linecap="round"
+                              />
+                            </svg>
+                            Scheduled to run
+                          </>
+                        )}
+                        <span
+                          style={{
+                            color: isCronExpressionValid ? "" : "#FF0000",
+                          }}
+                        >
+                          {description}
+                        </span>
+                      </Typography.Text>
+                    </Space>
+                  </Col>
+                </Row>
+              </Col>
+              <Col span={24}>
                 <Form.Item
                   className={ingestionCss.antFormItem}
-                  label={"Schedule"}
+                  label={"Time zone (Choose a time zone for the schedule)"}
                   labelAlign={"left"}
                   required
                 >
                   <Select
                     className={ingestionCss.inputSelect}
-                    value={selectedScheduleOption}
-                    placeholder={"select"}
-                    onChange={(option) => {
-                      setCronExpression("");
-                      setSelectedScheduleOption(option);
+                    style={{ width: "100%" }}
+                    showSearch
+                    value={scheduleTimezone}
+                    onChange={(value) => {
+                      setScheduleTimezone(value);
                     }}
-                    options={scheduleOption.map((val) => {
-                      return { label: val, value: val };
-                    })}
+                    options={timeZone}
                   />
                 </Form.Item>
               </Col>
-              {optionChild[selectedScheduleOption]}
-              <Col span={24} style={{ marginBottom: "8px" }}>
-                <Space size={8}>
-                  <Typography.Text className={ingestionCss.cronDescription}>
-                    {isCronExpressionValid && cronExpression !== "" && (
-                      <>
-                        <svg
-                          width="18"
-                          height="18"
-                          viewBox="0 0 18 18"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M5 9.17407L7.14645 11.4451C7.34171 11.6516 7.65829 11.6516 7.85355 11.4451L13 6M17 9C17 13.4183 13.4183 17 9 17C4.58172 17 1 13.4183 1 9C1 4.58172 4.58172 1 9 1C13.4183 1 17 4.58172 17 9Z"
-                            stroke="#309133"
-                            stroke-linecap="round"
-                          />
-                        </svg>
-                        Scheduled to run
-                      </>
-                    )}
-                    <span
-                      style={{ color: isCronExpressionValid ? "" : "#FF0000" }}
-                    >
-                      {description}
-                    </span>
-                  </Typography.Text>
-                </Space>
-              </Col>
-            </Row>
-          </Col>
-          <Col span={24}>
-            <Form.Item
-              className={ingestionCss.antFormItem}
-              label={"Time zone (Choose a time zone for the schedule)"}
-              labelAlign={"left"}
-              required
-            >
-              <Select
-                className={ingestionCss.inputSelect}
-                style={{ width: "100%" }}
-                showSearch
-                options={timeZone}
-              />
-            </Form.Item>
-          </Col>
+            </>
+          )}
           <Col span={24}>
             <Form.Item
               className={ingestionCss.antFormItem}
@@ -407,8 +553,15 @@ const Configure = ({ ingestionCss }) => {
                 className={ingestionCss.inputSelect}
                 style={{ width: "100%" }}
                 showSearch
-                options={scheduleOption.map((val) => {
-                  return { label: val, value: val };
+                value={selectedRuntimeEnvironmentId}
+                onChange={(value) => {
+                  setSelectedRuntimeEnvironmentId(value);
+                }}
+                options={environmentList.map((val) => {
+                  return {
+                    label: val?.runtime_env_name,
+                    value: val?.runtime_env_id,
+                  };
                 })}
               />
             </Form.Item>
@@ -530,7 +683,10 @@ const Configure = ({ ingestionCss }) => {
         <div className={ingestionCss.generalLastDiv}>
           <Space>
             <Button className={ingestionCss.defineSave}>Save & exit</Button>
-            <Button className={ingestionCss.defineSaveAndBuild}>
+            <Button
+              className={ingestionCss.defineSaveAndBuild}
+              onClick={handleSaveNDeploy}
+            >
               Save & Deploy
             </Button>
           </Space>
