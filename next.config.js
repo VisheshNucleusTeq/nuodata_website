d3561a6dc5d4fe3a88489a482a8f4b3306a92ac4
aishwary-nucleusteq <aishwary.rathore@nucleusteq.com>
2024-02-26 21:47:29 +0530

    sso login 1 --deploy

--- next.config.js
+++ next.config.js
@@ -5,11 +5,15 @@ const nextConfig = {
   env: {
     BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
     IS_LOCAL: process.env.IS_LOCAL,
+    AUTH0_SECRET: process.env.AUTH0_SECRET,
+    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
+    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
   },
   images: {
     minimumCacheTTL: 60,
   },
 };
-
 
 module.exports = nextConfig;
