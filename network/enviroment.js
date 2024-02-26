e38cace91f21ede7fd47d6a04ceb687678c6a57d
aishwary-nucleusteq <aishwary.rathore@nucleusteq.com>
2024-02-26 22:04:25 +0530

    sso login 4 --deploy

--- network/enviroment.js
+++ network/enviroment.js
@@ -86,6 +86,7 @@ BaseURL.interceptors.response.use(
 );
 
 const refreshToken = async () => {
+  return;
   try {
     const refreshToken = localStorage.getItem("refreshToken");
     const resp = await axios.post(`${process.env.BASE_URL}${GETACCESSTOKEN}`, {
