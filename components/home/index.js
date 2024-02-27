2e2c7fe79fe9c6e78e36101a72c0f68f3b665239
aishwary-nucleusteq <aishwary.rathore@nucleusteq.com>
2024-02-27 15:24:38 +0530

    latest --deploy

--- components/home/index.js
+++ components/home/index.js
@@ -6,6 +6,9 @@ import EnterpriseChallenge from "./enterprisechallenge";
 import ModernizeWithConfidence from "./modernizewithconfidence";
 import DataModernizationArchitecture from "./datamodernizationarchitecture";
 import SnapShot from "./snapShot";
+import DataInnovation from "./datainnovation";
+import UnifiedData from "./unifiedData";
+import TrustedInnovation from "./trustedInnovation";
 
 export default function Home() {
   return (
@@ -14,11 +17,39 @@ export default function Home() {
       <ThinkBig HomeCss={HomeCss} />
       <EnterpriseChallenge HomeCss={HomeCss} />
       <ModernizeWithConfidence HomeCss={HomeCss} />
-      <DataModernizationArchitecture HomeCss={HomeCss} />
-      <SnapShot
-        HomeCss={HomeCss}
-      />
+      {/* <DataModernizationArchitecture HomeCss={HomeCss} /> */}
+      <UnifiedData HomeCss={HomeCss} />
+      <SnapShot HomeCss={HomeCss} />
+      <TrustedInnovation HomeCss={HomeCss} />
+      <DataInnovation HomeCss={HomeCss} />
       <Footer />
     </>
   );
 }
+
+
+
+// import HomeCss from "../../styles/Home.module.css";
+// import Info from "./info";
+// import Footer from "../common/footer";
+// import ThinkBig from "./thinkbig";
+// import EnterpriseChallenge from "./enterprisechallenge";
+// import ModernizeWithConfidence from "./modernizewithconfidence";
+// import DataModernizationArchitecture from "./datamodernizationarchitecture";
+// import SnapShot from "./snapShot";
+
+// export default function Home() {
+//   return (
+//     <>
+//       <Info HomeCss={HomeCss} />
+//       <ThinkBig HomeCss={HomeCss} />
+//       <EnterpriseChallenge HomeCss={HomeCss} />
+//       <ModernizeWithConfidence HomeCss={HomeCss} />
+//       <DataModernizationArchitecture HomeCss={HomeCss} />
+//       <SnapShot
+//         HomeCss={HomeCss}
+//       />
+//       <Footer />
+//     </>
+//   );
+// }
