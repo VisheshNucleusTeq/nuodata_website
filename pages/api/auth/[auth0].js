6d52fb8b870259834d2911503f0785709eb0a6ad
aishwary-nucleusteq <aishwary.rathore@nucleusteq.com>
2024-02-27 17:14:31 +0530

    sso changes --deploy

--- pages/api/auth/[auth0].js
+++ pages/api/auth/[auth0].js
@@ -1,7 +1,19 @@
+// // import { handleAuth } from "@auth0/nextjs-auth0";
+// import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+// export default handleAuth({
+//   login: handleLogin({
+//     returnTo: "/sso/sso-login",
+//   }),
+// });
+
 // import { handleAuth } from "@auth0/nextjs-auth0";
 import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
-export default handleAuth({
+const auth = handleAuth({
   login: handleLogin({
     returnTo: "/sso/sso-login",
   }),
-});
+}); // Create authentication middleware using handleAuth function
+
+export default auth; // Export auth as the default export
+
+export { auth as GET, auth as POST }; // Export auth as named exports for both GET and POST methods
