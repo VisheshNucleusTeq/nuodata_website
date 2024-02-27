09673994628d2e6dae1353bf0eb772b2a7b3ce5b
aishwary-nucleusteq <aishwary.rathore@nucleusteq.com>
2024-02-27 15:00:21 +0530

    latest --deploy

--- components/home/datainnovation.js
+++ components/home/datainnovation.js
@@ -1,8 +1,16 @@
-import { Carousel as CarouselComponent, Col, Row, Button, Space } from "antd";
+import { Carousel as CarouselComponent, Col, Row, Button, Space, Image } from "antd";
 
 export default function DataInnovation({ HomeCss }) {
   return (
     <div className={HomeCss.innovationMainDiv}>
+      <div className={HomeCss.innovationRobotDiv}>
+        <Image
+          className={HomeCss.innovationRobotImg}
+          src="/data_innovation/3d-hand.png"
+          alt="banner"
+          preview={false}
+        />
+      </div>
       <Space
         direction="vertical"
         style={{
@@ -31,20 +39,8 @@ export default function DataInnovation({ HomeCss }) {
               className={HomeCss.innovationCol}
             >
               <Space direction="vertical" className={HomeCss.innovationSpace}>
-                <h1>$300M+</h1>
-                <h2>in enterprise savings</h2>
-              </Space>
-            </Col>
-            <Col
-              xs={22}
-              sm={12}
-              md={12}
-              lg={5}
-              className={HomeCss.innovationCol}
-            >
-              <Space direction="vertical" className={HomeCss.innovationSpace}>
                 <h1>10X</h1>
-                <h2>faster migrations</h2>
+                <h2>Performance Boost</h2>
               </Space>
             </Col>
             <Col
@@ -55,8 +51,8 @@ export default function DataInnovation({ HomeCss }) {
               className={HomeCss.innovationCol}
             >
               <Space direction="vertical" className={HomeCss.innovationSpace}>
-                <h1>10X</h1>
-                <h2>faster migrations</h2>
+                <h1>8X</h1>
+                <h2>Faster Migrations</h2>
               </Space>
             </Col>
             <Col
@@ -67,14 +63,26 @@ export default function DataInnovation({ HomeCss }) {
               className={HomeCss.innovationCol}
             >
               <Space direction="vertical" className={HomeCss.innovationSpace}>
-                <h1>40%</h1>
-                <h2>compute efficiency</h2>
+                <h1>50%</h1>
+                <h2>Cost Saving</h2>
+              </Space>
+            </Col>
+            <Col
+              xs={22}
+              sm={12}
+              md={12}
+              lg={5}
+              className={HomeCss.innovationCol}
+            >
+              <Space direction="vertical" className={HomeCss.innovationSpace}>
+                <h1>90%</h1>
+                <h2>Energy Cost Saving</h2>
               </Space>
             </Col>
             <Col xs={22} sm={12} md={12} lg={4}>
               <Space direction="vertical" className={HomeCss.innovationSpace}>
-                <h1>50%</h1>
-                <h2>reduction in data</h2>
+                <h1>90%</h1>
+                <h2>Energy Cost Saving</h2>
               </Space>
             </Col>
           </Row>
